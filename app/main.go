1da3192a6ff3d29b0b0e18f77c278729c2e3fc8f
kaankahveci <kaan.kahveci@krontech.com>
2026-08-12 14:55:27 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,31 +7,55 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+	fmt.Println("Before l.Accept(), does it block here?")
+
+	for {
+		conn, err := l.Accept()
+		fmt.Println("After l.Accept()")
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buff := make([]byte, 1024)
+	n, err := conn.Read(buff)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading from connection: ", err.Error())
 	}
 
+	if n < 12 {
+		fmt.Println("Received request is too short: ", n)
+		return
+	}
+
+	// Extract the correlation id from the request
+	// 0-3 [message_size - 4 bytes]
+	// 4-5 [request_api_key - 2 bytes]
+	// 6-7 [request_api_version - 2 bytes]
+	// 8-11 [correlation_id - 4 bytes]
+	correlationId := binary.BigEndian.Uint32(buff[8:12])
+
+	// 8 byte response
 	response := make([]byte, 8)
 	binary.BigEndian.PutUint32(response[0:4], 0)
-	binary.BigEndian.PutUint32(response[4:8], 7)
+	binary.BigEndian.PutUint32(response[4:8], correlationId)
 
 	_, err = conn.Write(response)
 	if err != nil {
-		fmt.Println("Error writing response: ", err.Error())
+		fmt.Println("Error writing to connection: ", err.Error())
+		return
 	}
 }
